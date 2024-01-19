097bf0882da339a2d3fbc51479dcf58e975f1230
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-20 02:47:47 +0600

    implemenint React & Explore rendering using root

--- reactDetails/main.js
+++ reactDetails/main.js
@@ -1,4 +1,15 @@
-console.log("Hello");
+// ---------- INstall REACT & REACTDOM Using CDN-----------
+// console.log("Hello");
+// console.log("React ==> ",React);
+// console.log("React DOM ==> ",ReactDOM);
 
-console.log("React ==> ",React);
-console.log("React DOM ==> ",ReactDOM);
+
+// -------- Creating ROOT----
+
+const App =  React.createElement('h1',{style: {color:"red"}},"HEllo Root")
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(App);
+
+console.log(root);
