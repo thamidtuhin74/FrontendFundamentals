2bead8b6deb294530b0b0f7f481d604cc6c08cfd
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-20 03:44:40 +0600

    adding Readme file

--- reactDetails/main.js
+++ reactDetails/main.js
@@ -4,29 +4,33 @@
 // console.log("React DOM ==> ",ReactDOM);
 
 
-// -------- Creating ROOT----
+
 
 //App without JSX👇;
-// const App =  React.createElement('h1',{style: {color:"red"}},"HEllo Root")
+const App =  React.createElement('h1',{style: {color:"red"}},"HEllo Root");
 
 
-const HelloWorld = () =>{
-    return <p>Hello World</p>
-}
-//App with JSX👇;
-const App = () =>{
-    return (
-        <React.Fragment>
-            <h1>Hello👋 This is A Simple Custom React App</h1>
-            <HelloWorld/>
-        </React.Fragment>
-    )
-}
 
 
+// const HelloWorld = () =>{
+//     return <p>Hello World</p>
+// }
+// //App with JSX👇;
+// const App = () =>{
+//     return (
+//         <React.Fragment>
+//             <h1>Hello👋 This is A Simple Custom React App</h1>
+//             <HelloWorld/>
+//         </React.Fragment>
+//     )
+// }
+
+// -------- Creating ROOT----
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(App);//Without JSX
-root.render(<App></App>);
+
+root.render(App);//Without JSX
+
+// root.render(<App></App>);
 
 console.log(root);
---
+++ readme.md
@@ -0,0 +1,19 @@
+# React Fundamentals
+
+## Install React -  Using CDN ("https://unpkg.com/react@18.2.0/umd/react.production.min.js")
+
+### Client side Rendering procedure of ReactJS
+- Ceate a 'root' in HTML file
+- React mainly return a Empty HTML file to a response
+- then Using JavaScript , Load Component to the HTML 'root'
+this is called Client side rendering.
+- For browser rendering react use ReactDOM (# CDN -'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js')
+
+## Bable -  Install Bable Using CDN for using JSX
+
+### Bable translate JSX to Raw JS code for the Browser. Because Browser doesn't understand JSX.
+
+## Modular Environment : Developer Friendly environment, where developer can code in multiple files. But It will minfie all the code for the User response.
+
+- Crate Modular ENdvironment using Bundeller.
+- Example of Bundeller : WebPack , RoolUp(vite)
